3e543944b38945f429ee39485bad3f1ba1b9d772
plumchang <yuhearts.gobl@gmail.com>
2024-09-12 10:18:35 +0900

    カニを描く移動パターンを追加

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Rust Hands On
 https://github.com/sukobuto/kani-life
+
+オプション引数
+- `なし` : 餌を取ったらすぐにペイント
+- `--kani` : 20ポイント貯めて、「カニ」を描く
+
+キー入力
+- `q` : 終了
--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client = Client::new();
     let stop_flag = Arc::new(Mutex::new(false));
 
+    // 引数判定
+    let args: Vec<String> = std::env::args().collect();
+    let kani_mode = args.contains(&"--kani".to_string());
+
     // キーボード入力待ちを別スレッドで行う
     let stop_flag_clone = Arc::clone(&stop_flag);
     tokio::spawn(async move {
@@ -53,78 +57,257 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut current_turn_side = Side::Right;
     let mut current_walk_side = Side::Left;
 
-    loop {
-        // qが押されたら終了
-        let stop_flag_value = stop_flag.lock().await;
-        if *stop_flag_value {
-            break;
-        }
-        drop(stop_flag_value);
+    if kani_mode {
+        // カニモード
+        println!("Kani mode");
 
-        // Scan
-        let scan_res = api::send_scan(&client, url, &token).await?;
+        let required_points = 20;
+        let mut total_points = 0;
 
-        match scan_res {
-            CommandResult::Scan { whatYouCanSee } => match whatYouCanSee {
-                WhatYouCanSee::Food => {
-                    println!("Found food");
+        // 必要なポイントを貯めるまでループ
+        while total_points < required_points {
+            // qが押されたら終了
+            let stop_flag_value = stop_flag.lock().await;
+            if *stop_flag_value {
+                break;
+            }
+            drop(stop_flag_value);
 
-                    // ターン
-                    api::send_turn(&client, url, &token, current_turn_side.clone()).await?;
-                }
-                WhatYouCanSee::Wall => {
-                    println!("Found wall");
+            let scan_res = api::send_scan(&client, url, &token).await?;
+            match scan_res {
+                CommandResult::Scan { whatYouCanSee } => match whatYouCanSee {
+                    WhatYouCanSee::Food => {
+                        println!("Found food");
 
-                    // ウォーク
-                    let walk_res = api::send_walk(&client, url, &token, current_walk_side.clone()).await?;
+                        // ターン
+                        api::send_turn(&client, url, &token, current_turn_side.clone()).await?;
+                    }
+                    WhatYouCanSee::Wall => {
+                        println!("Found wall");
 
-                    if let CommandResult::Walk {
-                        success,
-                        totalPoint,
-                        ..
-                    } = walk_res
-                    {
-                        if success {
-                            println!("Walked successfully: total_point={}", totalPoint);
+                        // ウォーク
+                        let walk_res =
+                            api::send_walk(&client, url, &token, current_walk_side.clone()).await?;
 
-                            // ポイントが1.0以上ならペイント
-                            if totalPoint >= 1.0 {
-                                let paint_res = api::send_paint(&client, url, &token).await?;
-
-                                if let CommandResult::Paint {
-                                    success,
-                                    totalPoint,
-                                    ..
-                                } = paint_res
-                                {
-                                    if success {
-                                        println!(
-                                            "Painted successfully: total_point={}",
-                                            totalPoint
-                                        );
-                                    } else {
-                                        println!("Paint failed: total_point={}", totalPoint);
-                                    }
-                                }
+                        if let CommandResult::Walk {
+                            success,
+                            totalPoint,
+                            ..
+                        } = walk_res
+                        {
+                            if success {
+                                total_points = totalPoint as usize;
+                                println!("Walked successfully: total_point={}", totalPoint);
                             } else {
-                                println!("Not enough point to paint: total_point={}", totalPoint);
-                            }
-                        } else {
-                            println!("Walk failed: total_point={}", totalPoint);
+                                println!("Walk failed: total_point={}", totalPoint);
 
-                            // 壁にぶつかったらターン
-                            api::send_turn(&client, url, &token, current_turn_side.clone()).await?;
+                                // 壁にぶつかったらターン
+                                api::send_turn(&client, url, &token, current_turn_side.clone())
+                                    .await?;
+                            }
                         }
                     }
-                }
-                WhatYouCanSee::Crab => {
-                    println!("Found crab");
-                }
-            },
-            _ => (),
+                    WhatYouCanSee::Crab => {
+                        println!("Found crab");
+                    }
+                },
+                _ => (),
+            }
+            sleep(Duration::from_millis(200)).await;
         }
 
-        sleep(Duration::from_millis(200)).await;
+        // 描画開始位置に移動
+        println!("Preparing to draw 'カニ'.");
+        // 左に9マス移動
+        for _ in 0..9 {
+            let walk_res = api::send_walk(&client, url, &token, Side::Left).await?;
+            if let CommandResult::Walk { success, .. } = walk_res {
+                if !success {
+                    break;
+                }
+            }
+            sleep(Duration::from_millis(200)).await;
+        }
+        // 右にターン
+        api::send_turn(&client, url, &token, Side::Right).await?;
+        // 左に10マス移動
+        for _ in 0..10 {
+            let walk_res = api::send_walk(&client, url, &token, Side::Left).await?;
+            if let CommandResult::Walk { success, .. } = walk_res {
+                if !success {
+                    break;
+                }
+            }
+            sleep(Duration::from_millis(200)).await;
+        }
+        // 左にターン
+        api::send_turn(&client, url, &token, Side::Left).await?;
+
+        // 「カニ」を5×5のドットで描く
+        let ka_dots = [
+            // カ
+            [0, 0, 1, 0, 0],
+            [1, 1, 1, 1, 1],
+            [0, 0, 1, 0, 1],
+            [0, 0, 1, 0, 1],
+            [0, 1, 0, 0, 1],
+        ];
+        let ni_dots = [
+            // ニ
+            [0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1],
+        ];
+        // 「カ」を描く
+        for i in 0..5 {
+            let is_even_row = i % 2 == 0;
+            // 偶数列は右に、奇数列は左に移動
+            if is_even_row {
+                current_walk_side = Side::Right;
+            } else {
+                current_walk_side = Side::Left;
+            }
+            for j in 0..5 {
+                // ペイント判定
+                if is_even_row {
+                    if ka_dots[i][j] == 1 {
+                        api::send_paint(&client, url, &token).await?;
+                    }
+                } else {
+                    if ka_dots[i][4 - j] == 1 {
+                        api::send_paint(&client, url, &token).await?;
+                    }
+                }
+                // 移動
+                if j < 4 {
+                    // 右に移動
+                    api::send_walk(&client, url, &token, current_walk_side.clone()).await?;
+                } else if i < 4 {
+                    // 下に移動
+                    api::send_turn(&client, url, &token, Side::Left).await?;
+                    api::send_walk(&client, url, &token, Side::Left).await?;
+                    api::send_turn(&client, url, &token, Side::Right).await?;
+                }
+                sleep(Duration::from_millis(200)).await;
+            }
+        }
+        // 「ニ」を描く
+        for i in 0..5 {
+            let is_even_row = i % 2 == 0;
+            // 偶数列は右に、奇数列は左に移動
+            if is_even_row {
+                current_walk_side = Side::Right;
+            } else {
+                current_walk_side = Side::Left;
+            }
+            for j in 0..5 {
+                // ペイント判定
+                if is_even_row {
+                    if ni_dots[i][j] == 1 {
+                        api::send_paint(&client, url, &token).await?;
+                    }
+                } else {
+                    if ni_dots[i][4 - j] == 1 {
+                        api::send_paint(&client, url, &token).await?;
+                    }
+                }
+                // 移動
+                if j < 4 {
+                    // 左右に移動
+                    api::send_walk(&client, url, &token, current_walk_side.clone()).await?;
+                } else if i < 4 {
+                    // 下に移動
+                    api::send_turn(&client, url, &token, Side::Left).await?;
+                    api::send_walk(&client, url, &token, Side::Left).await?;
+                    api::send_turn(&client, url, &token, Side::Right).await?;
+                }
+                sleep(Duration::from_millis(200)).await;
+            }
+        }
+    } else {
+        // 通常モード
+        println!("Normal mode");
+
+        loop {
+            // qが押されたら終了
+            let stop_flag_value = stop_flag.lock().await;
+            if *stop_flag_value {
+                break;
+            }
+            drop(stop_flag_value);
+
+            // Scan
+            let scan_res = api::send_scan(&client, url, &token).await?;
+
+            match scan_res {
+                CommandResult::Scan { whatYouCanSee } => match whatYouCanSee {
+                    WhatYouCanSee::Food => {
+                        println!("Found food");
+
+                        // ターン
+                        api::send_turn(&client, url, &token, current_turn_side.clone()).await?;
+                    }
+                    WhatYouCanSee::Wall => {
+                        println!("Found wall");
+
+                        // ウォーク
+                        let walk_res =
+                            api::send_walk(&client, url, &token, current_walk_side.clone()).await?;
+
+                        if let CommandResult::Walk {
+                            success,
+                            totalPoint,
+                            ..
+                        } = walk_res
+                        {
+                            if success {
+                                println!("Walked successfully: total_point={}", totalPoint);
+
+                                // ポイントが1.0以上ならペイント
+                                if totalPoint >= 1.0 {
+                                    let paint_res = api::send_paint(&client, url, &token).await?;
+
+                                    if let CommandResult::Paint {
+                                        success,
+                                        totalPoint,
+                                        ..
+                                    } = paint_res
+                                    {
+                                        if success {
+                                            println!(
+                                                "Painted successfully: total_point={}",
+                                                totalPoint
+                                            );
+                                        } else {
+                                            println!("Paint failed: total_point={}", totalPoint);
+                                        }
+                                    }
+                                } else {
+                                    println!(
+                                        "Not enough point to paint: total_point={}",
+                                        totalPoint
+                                    );
+                                }
+                            } else {
+                                println!("Walk failed: total_point={}", totalPoint);
+
+                                // 壁にぶつかったらターン
+                                api::send_turn(&client, url, &token, current_turn_side.clone())
+                                    .await?;
+                            }
+                        }
+                    }
+                    WhatYouCanSee::Crab => {
+                        println!("Found crab");
+                    }
+                },
+                _ => (),
+            }
+
+            sleep(Duration::from_millis(200)).await;
+        }
     }
 
     // Rawモードを解除して通常の画面に戻す
